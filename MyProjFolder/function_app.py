66c43103b6a8e9f135945b4fdc1adf466bdc87de
FluMcqueen <flavio.faelli@itsrizzoli.it>
2024-11-06 13:57:28 +0000

    prova git

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -3,48 +3,12 @@ import json
 import logging
 import requests
 from bs4 import BeautifulSoup
-import fitz 
 import re
 import pandas as pd
 
 # Altro codice :)
 
 app = func.FunctionApp()
-
-def get_pdf_link(ingredient_id):
-    url = f"https://cir-reports.cir-safety.org/cir-ingredient-status-report/?id={ingredient_id}"
-    response = requests.get(url).text
-    soup = BeautifulSoup(response, "lxml")
-    tab = soup.find("table")
-    attach = tab.find("a")
-    pidieffe = attach["href"]
-    linktr = str(pidieffe).replace("../", "")
-    pdf_link = "https://cir-reports.cir-safety.org/" + linktr
-    return pdf_link
-
-def extract_text_from_pdf_url(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            pdf_data = response.content
-            text_pages = []
-            document = fitz.open(stream=pdf_data, filetype="pdf")
-            for page_num in range(len(document)):
-                try:
-                    page = document.load_page(page_num)
-                    page_text = page.get_text()
-                    if page_text:
-                        text_pages.append((page_text, page_num + 1))
-                    else:
-                        print(f"Nessun testo trovato nella pagina {page_num + 1}")
-                except Exception as e:
-                    print(f"Errore durante l'estrazione del testo dalla pagina {page_num + 1}: {str(e)}")
-            return text_pages
-        else:
-            print(f"Errore durante l'apertura del PDF. Codice di stato: {response.status_code}")
-    except Exception as e:
-        print(f"Errore generale durante l'operazione di estrazione del testo dal PDF: {str(e)}")
-
 
 def extract_noael_and_ld50(text_pages):
     noael_pattern = re.compile(r'(.*?NOAEL.*?\d+\.?\d*\s*[a-zA-Z/]+.*?(\.|$))', re.IGNORECASE)
@@ -93,32 +57,22 @@ def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
             ingrediente = req_body.get('ingrediente')
 
     if ingrediente:
-        with open('cirjs.json', 'r') as file:
-            cir = json.load(file)
-        lista_ing = list(cir.keys())
-        ingredient_id = cir.get(ingrediente)
+        with open('invecchia.json', 'r') as file:
+            echa = json.load(file)
+        lista_ing = list(echa.keys())
 
-        if ingredient_id:
-            pdf_link = get_pdf_link(ingredient_id)
+        urlecha = echa.get(ingrediente)
+        if urlecha:
+            echalink = "https://echa.europa.eu/it/registration-dossier/-/registered-dossier/"+str(urlecha)+"/7/1"
+            print(f"Link al dossier Echa: [Clicca qui per visualizzare il dossier]({echalink})")
+            response = requests.get(echalink)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            dl = echadnel(soup)
+            nl = echanoael(soup)
 
-            try:
-                text_pages = extract_text_from_pdf_url(pdf_link)
-                        
-                noael_matches, ld50_matches = extract_noael_and_ld50(text_pages)
-                        
-                if noael_matches:
-                    noael_df = pd.DataFrame(noael_matches, columns=["NOAEL value", "Page"])
-                    print("NOAEL")
-                        
-                if ld50_matches:
-                    ld50_df = pd.DataFrame(ld50_matches, columns=["LD50 value", "Page"])
-                    print("LD50")
-                        
-                if not noael_matches and not ld50_matches:
-                    print("Nessun valore NOAEL o LD50 trovato.")                            
-                        
-            except Exception as e:
-                return (f"ERRORE: {e}")
+            noael_matches = [(highlight_numbers(noael),) for noael in nl]
+            dnel_matches = [(highlight_numbers(dnel),) for dnel in dl]
+            display_echa_results(noael_matches, dnel_matches)
 
         return func.HttpResponse(f"Hello, {ingrediente}. This HTTP triggered function executed successfully.")
     else:
